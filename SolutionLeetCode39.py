aa25b282489baf40c314549ec6877c867ee7c5ac
HumbertoBPF <humbertobpf@gmail.com>
2022-04-12 17:19:52 -0300

    Improving code of question 39 and 40 (no need to use a deque, we can use a pointer instead)

--- SolutionLeetCode39.py
+++ SolutionLeetCode39.py
@@ -1,36 +1,31 @@
-from collections import deque
 from typing import List
 
 
 class Solution:
 
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        return self.get_combinations(deque(candidates), 0, target, [], [])
+        return self.get_combinations(candidates, 0, 0, target, [], [])
 
-    # Remark: the numbers_to_use argument is used to specify to the function the candidates that must participate of the
-    # combination list that is being built
-    def get_combinations(self, numbers_to_use, current_sum, target, combination, combinations):
+    def get_combinations(self, candidates, index, current_sum, target, combination, combinations):
         # If the current sum of the items of the array "combinations" is greater or equal than the target one, stop this recursion
         if current_sum >= target:
             # If the target value is attained, append this combination to the list of combinations
             if current_sum == target:
                 combinations.append(combination)
             return combinations
-        # Find the combinations containing the items that still are in the queue. Remark: the number of combinations is
-        # always lower then 150
-        while len(numbers_to_use) != 0 and len(combinations) < 150:
-            # Before removing the first item from the queue, create a copy of the queue, which is going to be used in the
-            # next call of the recursion
-            numbers_to_use_copy = numbers_to_use.copy()
-            # Remove the first item of the queue and add it to the current combination and to the current sum
-            current_number = numbers_to_use.popleft()
-            combination_copy = combination.copy()
-            combination_copy.append(current_number)
-            # Call recursion to keep trying to reach the target sum
-            self.get_combinations(numbers_to_use_copy, current_sum + current_number, target, combination_copy, combinations)
+        # Find the combinations containing the items that still are after combinations[index](included).
+        # Remark: the number of combinations is always lower then 150
+        while index != len(candidates) and len(combinations) < 150:
+            # Pick the item currently being pointed and add it to the current combination and to the current sum
+            current_number = candidates[index]
+            # Call recursion to keep trying to reach the target sum(notice that we have not skipped the item being currently
+            # pointed since repetitions are allowed)
+            self.get_combinations(candidates, index, current_sum + current_number, target, combination+[current_number], combinations)
+            # Go to the next item
+            index += 1
 
         return combinations
 
 
 o1 = Solution()
-print(o1.combinationSum([2,3,5], 8))
+print(o1.combinationSum([2,3,6,7], 7))
--- SolutionLeetCode40.py
+++ SolutionLeetCode40.py
@@ -1,35 +1,32 @@
-from collections import deque
 from typing import List
 
 
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         candidates.sort()
-        return self.get_combinations(deque(candidates), 0, target, [], [])
+        return self.get_combinations(candidates, 0, 0, target, [], [])
 
-    def get_combinations(self, numbers_to_use, current_sum, target, combination, combinations):
+    def get_combinations(self, candidates, index, current_sum, target, combination, combinations):
         # Stop recursion if the sum has been reached
         if current_sum >= target:
             # If the sum of the combination's items is equal to the target, add the combination to the list of combinations
             if current_sum == target:
                 combinations.append(combination)
             return combinations
-        # Try to use all the numbers of the input queue without repeating the same number
+        # Try to use all the numbers of candidates without repeating the same number
         last_number = -1
-        while len(numbers_to_use) != 0:
-            # Pop the number from the queue when it is added to the current combination
-            current_number = numbers_to_use.popleft()
+        while index != len(candidates):
+            # Point to the next item of candidates when the item currently being pointed is added to the current combination
+            current_number = candidates[index]
+            index += 1
             # If a repetition is identified, skip this number
             if current_number == last_number:
                 continue
-            # Create a copy to avoid some kind of "persistence effect" through the multiple recursion
-            combination_copy = combination.copy()
-            combination_copy.append(current_number)
-            self.get_combinations(numbers_to_use.copy(), current_sum + current_number, target, combination_copy, combinations)
+            self.get_combinations(candidates, index, current_sum + current_number, target, combination + [current_number], combinations)
             last_number = current_number
 
         return combinations
 
 
 o1 = Solution()
-print(o1.combinationSum2([2, 5, 2, 1, 2], 5))
+print(o1.combinationSum2([10,1,2,7,6,1,5], 8))
